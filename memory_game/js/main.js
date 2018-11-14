ce0f66841d24453d78f5461d4b82989a6fb59489
Scott McCormick <scott.t.mccormick@gmail.com>
2018-11-14 10:32:24 -0800

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped", cardOne);
+console.log("User flipped", cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
