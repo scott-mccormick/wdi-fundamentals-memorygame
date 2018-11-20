e165a8149f0170945ac81eff32247b8e5e226656
Scott McCormick <scott.t.mccormick@gmail.com>
2018-11-20 15:37:51 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -16,24 +16,56 @@ let cards = [{
 	cardImage: "images/king-of-diamonds.png"
 }];
 let cardsInPlay = [];
+let userScore = -1;
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
+		updateScore();
 	} else {
 		alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
 	cardsInPlay.push(cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	this.setAttribute("src", cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
+	if (cardsInPlay.length === 1) {
+		document.getElementById('reset-btn').classList.toggle('hidden');
+	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	var resetButton = document.getElementById('reset-btn');
+	resetButton.addEventListener('click', resetGame);
+
+	updateScore();
+}
+
+function resetGame() {
+	var gameCards = document.querySelectorAll('#game-board img');
+	for(var i = 0; i < gameCards.length; i++){
+		gameCards[i].setAttribute('src', 'images/back.png');
+	}
+	cardsInPlay = [];
+	document.getElementById('reset-btn').classList.toggle('hidden');
+}
+
+function updateScore() {
+	userScore++;
+	var scoreBoard = document.getElementById('user-score');
+	scoreBoard.innerHTML= userScore;
+}
+
+createBoard();
